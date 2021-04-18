8d1cdd034de37c4e36df52150f777c4768153534
Yanyu Liang <yanyul@uchicago.edu>
2021-04-18 12:13:52 -0500

    working on geno cov for gtex v8

---
+++ SPrediXcan2PTRS/geno_cov/cov_constructor.py
@@ -0,0 +1,174 @@
+import numpy as np
+from scipy.sparse import coo_matrix, save_npz, load_npz
+
+class CovConstructor:
+    def __init__(self, data, nbatch=None):
+        '''
+        Important: self.data is ALWAYS centered
+        '''
+        self.data = data - data.mean(axis=0)
+        self.ncol = self.data.shape[1]
+        self.nrow = self.data.shape[0]
+        self.nbatch = nbatch
+        self._set_batch_anchors()
+    def _set_batch_anchors(self):
+        ncol = self.ncol
+        batch_size = ncol // self.nbatch
+        if batch_size * self.nbatch < ncol:
+            batch_size += 1
+        if batch_size < 10 and self.nbatch > 5:
+            raise ValueError('Too many batches. Exit.')
+        self.batch_anchors = []
+        for i in range(self.nbatch):
+            start = i * batch_size
+            end = min((i + 1) * batch_size, ncol)
+            self.batch_anchors.append([start, end])
+    def _flatten_2d_mat(self, mat):
+        row_index_mat = np.tile(np.arange(mat.shape[0]), reps=(mat.shape[1], 1)).T
+        row = row_index_mat.flatten()
+        del row_index_mat
+        col_index_mat = np.tile(np.arange(mat.shape[1]), reps=(mat.shape[0], 1))
+        col = col_index_mat.flatten()
+        del col_index_mat
+        return row, col, mat.flatten()
+    def compute_to_h5(self, fn, dtype='f'):
+        import h5py
+        f = h5py.File(fn, 'w')
+        dset = f.create_dataset('cov', (self.ncol, self.ncol), dtype=dtype)
+        for i, (s1, e1) in enumerate(self.batch_anchors):
+            for j, (s2, e2) in enumerate(self.batch_anchors):
+                if i > j:
+                    continue
+                dset[s1 : e1, s2 : e2] = self._compute_cov(
+                    s1, e1, s2, e2, 
+                    flatten=False, triu=(i == j)
+                )
+                tmp = self._compute_cov(
+                    s1, e1, s2, e2, 
+                    flatten=False, triu=(i == j)
+                )
+        f.close()
+    def compute_to_disk(self, mode, output_prefix, param=None):
+        if mode == 'naive':
+            fn = output_prefix + '.naive.h5'
+            self.compute_to_h5(fn, dtype=param)
+        elif mode == 'cap':
+            fn = output_prefix + '.cap.npz'
+            self.compute_to_cap_npz(fn, threshold=param)
+        elif mode == 'banded':
+            fn = output_prefix + '.banded.npz'
+            self.compute_to_banded_npz(fn, band_size=param)
+    def _compute_cov(self, s1, e1, s2, e2, flatten=True, triu=True):
+        '''
+        Given submatrix index: 
+            matrix1 = [:, s1 : e1], matrix2 = [:, s2 : e2]
+        Return: 
+            Pairwise covariance between column in matrix1 and column in matrix2.
+            Elements are returned in row, col, val format (flatten = True). 
+            And only row <= col ones are returned.
+            But if flatten = False, triu could be set to False
+            to return the full matrix.
+        Formula:
+            covariance = col1 * col2 / self.nrow (col is centered in __init__) 
+        '''
+        tmp = np.einsum('ni,nj->ij', self.data[:, s1 : e1], self.data[:, s2 : e2]) / (self.nrow - 1)
+        if flatten is False:
+            if triu is True:
+                return np.triu(tmp)
+            else:
+                return tmp
+        row, col, val = self._flatten_2d_mat(tmp)
+        row += s1
+        col += s2
+        to_keep = row <= col
+        row, col, val = row[to_keep], col[to_keep], val[to_keep]
+        del tmp
+        return row, col, val
+    def compute_to_banded_npz(self, fn, band_size=100):
+        row_all, col_all, value_all = [], [], []
+        for i, (s1, e1) in enumerate(self.batch_anchors):
+            for j, (s2, e2) in enumerate(self.batch_anchors):
+                if i > j:
+                    continue
+                if s2 > e1 + band_size - 1:
+                    continue
+                row, col, value = self._compute_cov(s1, e1, s2, e2)
+                to_keep = col - row <= band_size 
+                row, col, value = row[to_keep], col[to_keep], value[to_keep]
+                row_all.append(row)
+                col_all.append(col)
+                value_all.append(value)
+        row_all = np.concatenate(row_all, axis=0)
+        col_all = np.concatenate(col_all, axis=0)
+        value_all = np.concatenate(value_all, axis=0)
+        cov_coo = coo_matrix(
+            (value_all, (row_all, col_all)), 
+            shape=(self.ncol, self.ncol)
+        )
+        save_npz(fn, cov_coo)  
+    def compute_to_cap_npz(self, fn, threshold=1e-5):
+        row_all, col_all, value_all = [], [], []
+        for i, (s1, e1) in enumerate(self.batch_anchors):
+            for j, (s2, e2) in enumerate(self.batch_anchors):
+                if i > j:
+                    continue
+                row, col, value = self._compute_cov(s1, e1, s2, e2)
+                to_keep = np.abs(value) > threshold
+                row, col, value = row[to_keep], col[to_keep], value[to_keep]
+                row_all.append(row)
+                col_all.append(col)
+                value_all.append(value)
+        row_all = np.concatenate(row_all, axis=0)
+        col_all = np.concatenate(col_all, axis=0)
+        value_all = np.concatenate(value_all, axis=0)
+        cov_coo = coo_matrix(
+            (value_all, (row_all, col_all)), 
+            shape=(self.ncol, self.ncol)
+        )
+        save_npz(fn, cov_coo)    
+
+class CovMatrix:
+    def __init__(self, fn):
+        self.fn = fn
+        self.mode = self._init_mode()
+    def _init_mode(self):
+        import pathlib
+        if not pathlib.Path(self.fn).is_file():
+            raise ValueError('Input file does not exist.') 
+        tmp = self.fn.split('.')
+        return tmp[-2]
+    def eval_matmul_on_left(self, left_mat, param=None):
+        '''
+        Retur cov @ left_mat along with the diag 
+        '''
+        if self.mode in ['banded', 'cap']:
+            return self._eval_matmul_on_left_npz(left_mat)
+        elif self.mode == 'naive':
+            return self._eval_matmul_on_left_h5(left_mat, batch_size=param)
+    def _eval_matmul_on_left_npz(self, mat):
+        csr = load_npz(self.fn).tocsr()
+        diag_csr = csr.diagonal()
+        return csr.dot(mat) + csr.transpose().dot(mat) - diag_csr[:, np.newaxis] * mat, diag_csr
+    def _eval_matmul_on_left_h5(self, mat, batch_size=None):
+        import h5py
+        f = h5py.File(self.fn, 'r')
+        nrow = f['cov'].shape[0]
+        ncol = mat.shape[1]
+        res = np.zeros((nrow, ncol))
+        if batch_size is None:
+            nbatch = 1
+            batch_size = nrow
+        else:
+            nbatch = nrow // batch_size
+            if nbatch * batch_size < nrow:
+                nbatch += 1
+        s, e = 0, batch_size
+        diag_cov = np.zeros((nrow))
+        for i in range(nbatch):
+            res[s : e, :] = f['cov'][s : e, :] @ mat
+            res[s : e, :] += f['cov'][:, s : e].T @ mat
+            diag_cov[s : e] = f['cov'][s : e, s : e].diagonal()
+        res -= diag_cov[:, np.newaxis] * mat
+        f.close()
+        return res, diag_cov
+        
---
+++ SPrediXcan2PTRS/geno_cov/gtex_v8_loader.py
@@ -0,0 +1,134 @@
+from collections import OrderedDict
+
+import cyvcf2 as cy
+import sqlite3 as sq
+import pandas as pd
+import numpy as np
+
+
+COMPLEMENT_BASE = {
+    'A': 'T',
+    'G': 'C',
+    'T': 'A',
+    'C': 'G'
+}
+VARID_SEP = '_'
+
+def get_complement(str_):
+    o = ''
+    for s in str_:
+        char_ = s.upper()
+        if char_ not in COMPLEMENT_BASE:
+            raise ValueError(f'Wrong s in str_: s = {s}.')
+        o += COMPLEMENT_BASE[char_]
+    return o
+
+
+class TargetSNP:
+    def __init__(self, df_snp):
+        self._if_unique_chr_pos(df_snp)
+        self._init_snp_dict(df_snp)
+    def _init_snp_dict(self, df_snp):
+        self.snp_dict = OrderedDict()
+        for chr, pos, ref, alt in zip(df_snp.chr, df_snp.pos, df_snp.ref, df_snp.alt):
+            kk = self.gen_chrpos(chr, pos)
+            if kk not in self.snp_dict:
+                self.snp_dict[kk] = []
+            self.snp_dict[kk].append(
+                pd.DataFrame({ 
+                    'chr': [chr], 'pos': [pos], 
+                    'ref': [ref], 'alt': [alt]
+                })
+            )
+    @staticmethod
+    def _try_match(a1, a2, b1, b2):
+        if a1 == b1 and a2 == b2:
+            return 1
+        if a1 == get_complement(b1) and a2 == get_complement(b2):
+            return 1
+        if a1 == b2 and a2 == b1:
+            return -1
+        if a1 == get_complement(b2) and a2 == get_complement(b1):
+            return -1
+        return 0
+    def get_snp(self, key_, ref, alt):
+        if key_ not in self.snp_dict:
+            return None, None
+        snps = self.snp_dict[key_]
+        for i in range(snps.shape[0]):
+            res = self._try_match(snps.ref[i], snps.alt[i], ref, alt)
+            if res == 0:
+                continue
+            else:
+                return snps.iloc[i:(i + 1), :].copy(), res
+        return None, None
+    @staticmethod
+    def gen_chrpos(chr, pos):
+        return f'{chr}_x_{pos}'
+            
+        
+
+class GTExV8GenoLoader:
+    def __init__(self, geno):
+        self.vcf = cy.VCF(geno)
+    def load(self, df_snp):
+        '''
+        df_snp = pd.DataFrame({
+            'chr': chromosome,
+            'pos': position,
+            'ref': reference allele,
+            'alt': alternative allele
+        })
+        Return the genotype matrix (snp x sample)
+        with matched alleles 
+        '''
+        chrs = df_snp.chr.unique()
+        geno_mat = []
+        snps = []
+        for cc in chrs:
+            df_snp_sub = df_snp[ df_snp.chr == cc ]
+            start = df_snp_sub.pos.min()
+            end = df_snp_sub.pos.max()
+            snps_sub, geno_mat_sub = self._load_region(f'{cc}:{start - 1}:{end}', target_snps=df_snp_sub)
+            geno_mat.append(geno_mat_sub)
+            snps.append(snps_sub)
+        geno_mat = np.concatenate(geno_mat, axis=0)
+        snps = pd.concat(snps, axis=0)
+        return geno_mat, snps
+    def _load_region(self, region, target_snps):
+        snp_target = TargetSNP(target_snps)
+        snps = []
+        geno_mat = []
+        for kk in self.vcf(region):
+            ch, pos, ref, alt = kk.CHROM, kk.POS, kk.REF, kk.ALT
+            kk_key = snp_target.gen_chrpos(ch, pos)
+            snpi, direction = snp_target.get_snp(kk_key, ref, alt)
+            if snpi is None:
+                continue
+            else:
+                mat = np.array(kk.genotypes)[:, :2].sum(axis=1)
+                if direction == -1:
+                    mat = 2 - mat
+                geno_mat.append(mat)
+                snps.append(snpi)
+        snps = pd.concat(snps, axis=0)
+        geno_mat = pd.concat(geno_mat, axis=0)
+        return geno_mat, snps
+            
+
+class GTExV8DBLoader:
+    def __init__(self, db):
+        with sq.connect(db) as conn
+            df_weight = pd.read_sql_query('select * from weights', conn)
+        df_weight['chr'], df_weight['pos'] = self._parse_varID(df_weight.varID)
+        self.df_weight = df_weight
+    @staticmethod
+    def _parse_varID(slist):
+        cc = []
+        pp = []
+        for s in slist:
+            tmp = s.split(VARID_SEP)
+            cc.append(tmp[0])
+            pp.append(int(tmp[1]))
+        return cc, pp
+            
