bfc8d38aae38b8f2da69ca7f5ade4565e4106998
Yanyu Liang <yanyul@uchicago.edu>
2021-04-22 12:01:56 -0500

    added submission script

--- misc_scripts/test_ptrs_ukb_gwas/run_ukb_gwas_to_ptrs.sh
+++ misc_scripts/test_ptrs_ukb_gwas/run_ukb_gwas_to_ptrs.sh
@@ -1,8 +1,27 @@
-db_file=/gpfs/data/im-lab/nas40t2/Data/PredictDB/GTEx_v8/models_v1/eqtl/ctimp/ctimp_Whole_Blood.db
+#PBS -S /bin/bash
+#PBS -l walltime=24:00:00
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=32gb
+#PBS -e run_ukb_gwas_to_ptrs.err
+#PBS -o run_ukb_gwas_to_ptrs.out
+
+
+source ~/.bash_profile
+source ~/.bashrc
 
 # run s-predixcan
 conda activate imlabtools
 SNMK=/gpfs/data/im-lab/nas40t2/yanyul/softwares/miniconda2/envs/snakemake/bin/snakemake
 snmkscript=/gpfs/data/im-lab/nas40t2/yanyul/GitHub/misc-tools/spredixcan_workflow/spredixcan_pipe.snmk
 
-$SNMK -s $snmkscript --configfile config.spxcan.yaml -np
+
+outfile=/scratch/t.cri.yliang/SPrediXcan2PTRS/test_ptrs_ukb_gwas/spredixcan.height_x_Whole_Blood.harmonization_full.csv 
+# db_file=/gpfs/data/im-lab/nas40t2/Data/PredictDB/GTEx_v8/models_v1/eqtl/ctimp/ctimp_Whole_Blood.db
+if [[ ! -f $outfile ]]
+then
+  $SNMK -s $snmkscript --configfile ${PBS_O_WORKDIR}/config.spxcan.yaml -p \
+    > ${PBS_O_WORKDIR}/run_ukb_gwas_to_ptrs.spredixcan.log 2> &1
+fi
+
+# run SPrediXcan2PTRS
+# TODO
