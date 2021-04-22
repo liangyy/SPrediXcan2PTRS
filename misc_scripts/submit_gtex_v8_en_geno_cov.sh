425f209e079c5141c1d9a0efcd4e0c35ef18a14e
Yanyu Liang <yanyul@uchicago.edu>
2021-04-22 09:36:39 -0500

    added ctimp geno cov

--- misc_scripts/submit_gtex_v8_en_geno_cov.sh
+++ misc_scripts/submit_gtex_v8_en_geno_cov.sh
@@ -1,4 +1,6 @@
+# args1: tag=en|ctimp
+tag=$1
 for tissue in `cat gtex_v8_tissue_list.txt`
 do
-  qsub -v TISSUE=$tissue -N en_$tissue gtex_v8_en_geno_cov.qsub
+  echo qsub -v TISSUE=$tissue -N ${tag}_$tissue gtex_v8_${tag}_geno_cov.qsub
 done
