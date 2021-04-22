b5d82b4ce711729c6a629ed4554e6825a722e128
Yanyu Liang <yanyul@uchicago.edu>
2021-04-22 16:37:17 -0500

    added run cmd in test

--- misc_scripts/test_ptrs_ukb_gwas/run_ukb_gwas_to_ptrs.sh
+++ misc_scripts/test_ptrs_ukb_gwas/run_ukb_gwas_to_ptrs.sh
@@ -6,6 +6,7 @@
 #PBS -o run_ukb_gwas_to_ptrs.out
 #PBS -N run_ukb_gwas_to_ptrs
 
+
 source ~/.bash_profile
 source ~/.bashrc
 
@@ -15,13 +16,57 @@ SNMK=/gpfs/data/im-lab/nas40t2/yanyul/softwares/miniconda2/envs/mixqtl/bin/snake
 snmkscript=/gpfs/data/im-lab/nas40t2/yanyul/GitHub/misc-tools/spredixcan_workflow/spredixcan_pipe.snmk
 
 
-outfile=/scratch/t.cri.yliang/SPrediXcan2PTRS/test_ptrs_ukb_gwas/spredixcan.height_x_Whole_Blood.harmonization_full.csv 
+pxcan_file=/scratch/t.cri.yliang/SPrediXcan2PTRS/test_ptrs_ukb_gwas/spredixcan.height_x_Whole_Blood.harmonization_full.csv 
+gwas_file=/scratch/t.cri.yliang/SPrediXcan2PTRS/test_ptrs_ukb_gwas/height.harmonization_full.txt.gz
 # db_file=/gpfs/data/im-lab/nas40t2/Data/PredictDB/GTEx_v8/models_v1/eqtl/ctimp/ctimp_Whole_Blood.db
-if [[ ! -f $outfile ]]
+if [[ ! -f $pxcan_file ]]
 then
   $SNMK -s $snmkscript --configfile ${PBS_O_WORKDIR}/config.spxcan.yaml -p --cores 1 \
     > ${PBS_O_WORKDIR}/run_ukb_gwas_to_ptrs.spredixcan.log 2>&1
 fi
 
 # run SPrediXcan2PTRS
-# TODO
+
+export PYTHONPATH=$PYTHONPATH:/home/t.cri.yliang/labshare/GitHub/SPrediXcan2PTRS
+export PYTHONPATH=$PYTHONPATH:/home/t.cri.yliang/labshare/GitHub/transethnic_prs
+
+runscript=/home/t.cri.yliang/labshare/GitHub/SPrediXcan2PTRS/run_pxcan2ptrs.py
+
+geno_cov_file=/home/t.cri.yliang/scratch/SPrediXcan2PTRS/geno_cov/gtex_v8/ctimp_Whole_Blood.geno_cov.chr{chr_num}.evd.npz
+db_file=/gpfs/data/im-lab/nas40t2/Data/PredictDB/GTEx_v8/models_v1/eqtl/ctimp/ctimp_Whole_Blood.db
+
+outdir=/scratch/t.cri.yliang/SPrediXcan2PTRS/test_ptrs_ukb_gwas
+outprefix=SPrediXcan2PTRS.height_x_Whole_Blood.harmonization_full 
+outprefix2=SPrediXcan2PTRS.height_x_Whole_Blood.harmonization_full.2 
+
+chainfile=/gpfs/data/im-lab/nas40t2/yanyul/data/MetaXcan/data/liftover/hg19ToHg38.over.chain.gz
+
+# test 1
+if [[ $1 == 1 ]]
+then
+  echo python $runscript \
+    --predixcan $pxcan_file \
+    --predictdb $db_file \
+    --geno_cov $geno_cov_file \
+    --gwas $gwas_file \
+    --gwas_cols chromosome=chromosome \
+      position=position \
+      effect_allele=effect_allele \
+      non_effect_allele=non_effect_allele \
+    --gwas_sample_size 360388 \
+    --output_prefix $outdir/$outprefix
+fi
+
+# test 2
+if [[ $1 == 2 ]]
+then
+  echo python $runscript \
+    --predixcan $pxcan_file \
+    --predictdb $db_file \
+    --geno_cov $geno_cov_file \
+    --gwas $gwas_file \
+    --gwas_cols variant={chromosome}:{position}:{non_effect_allele}:{effect_allele} \
+    --liftover_chain $chainfile \
+    --gwas_sample_size 360388 \
+    --output_prefix $outdir/$outprefix2
+fi
