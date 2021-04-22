702ba937d86fbe3e8bee58d339d5cb41df97f76d
Yanyu Liang <yanyul@uchicago.edu>
2021-04-22 16:29:25 -0500

    changed to use neale gwas

--- misc_scripts/test_ptrs_ukb_gwas/run_ukb_gwas_to_ptrs.sh
+++ misc_scripts/test_ptrs_ukb_gwas/run_ukb_gwas_to_ptrs.sh
@@ -4,14 +4,14 @@
 #PBS -l mem=32gb
 #PBS -e run_ukb_gwas_to_ptrs.err
 #PBS -o run_ukb_gwas_to_ptrs.out
-
+#PBS -N run_ukb_gwas_to_ptrs
 
 source ~/.bash_profile
 source ~/.bashrc
 
 # run s-predixcan
 conda activate imlabtools
-SNMK=/gpfs/data/im-lab/nas40t2/yanyul/softwares/miniconda2/envs/snakemake/bin/snakemake
+SNMK=/gpfs/data/im-lab/nas40t2/yanyul/softwares/miniconda2/envs/mixqtl/bin/snakemake
 snmkscript=/gpfs/data/im-lab/nas40t2/yanyul/GitHub/misc-tools/spredixcan_workflow/spredixcan_pipe.snmk
 
 
@@ -19,8 +19,8 @@ outfile=/scratch/t.cri.yliang/SPrediXcan2PTRS/test_ptrs_ukb_gwas/spredixcan.heig
 # db_file=/gpfs/data/im-lab/nas40t2/Data/PredictDB/GTEx_v8/models_v1/eqtl/ctimp/ctimp_Whole_Blood.db
 if [[ ! -f $outfile ]]
 then
-  $SNMK -s $snmkscript --configfile ${PBS_O_WORKDIR}/config.spxcan.yaml -p \
-    > ${PBS_O_WORKDIR}/run_ukb_gwas_to_ptrs.spredixcan.log 2> &1
+  $SNMK -s $snmkscript --configfile ${PBS_O_WORKDIR}/config.spxcan.yaml -p --cores 1 \
+    > ${PBS_O_WORKDIR}/run_ukb_gwas_to_ptrs.spredixcan.log 2>&1
 fi
 
 # run SPrediXcan2PTRS
